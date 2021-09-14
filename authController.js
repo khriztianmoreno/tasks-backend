89c0e5aa68e6e5f7f0bf2d1d6a2ecc9dcaab9938
Germán Escobar <german.escobarc@gmail.com>
2021-09-14 10:09:39 -0500

    Create new auth route to validate token

---
+++ authController.js
@@ -0,0 +1,29 @@
+const User = require('./user');
+var jwt = require('jsonwebtoken');
+
+const login = async (req, res) => {
+  // recibir credenciales (email y contraseña) y retornar un JWT
+  const { email, password } = req.body;
+
+  const user = await User.authenticate(email, password);
+  if (user) {
+    // retornamos el JWT
+    const token = jwt.sign({ 
+      exp: Math.floor(Date.now() / 1000) + (15 * 24 * 60 * 60),
+      userId: user._id 
+    }, 'secret key');
+    res.json({ token });
+  } else {
+    res.status(401).json({ error: "invalid-credentials", message: 'Credenciales inválidas' });
+  }
+}
+
+const me = async (req, res) => {
+  const { email, firstName, lastName } = res.locals.user
+  res.json({ email, firstName, lastName })
+}
+
+module.exports = {
+  login,
+  me
+}
--- routes.js
+++ routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
-var jwt = require('jsonwebtoken');
 const tasksController = require('./tasksController');
+const authController = require('./authController');
 const { auth } = require('./middlewares');
-const User = require('./user');
 
 const app = express.Router();
 
@@ -10,21 +9,7 @@ app.get('/tasks', auth, tasksController.list);
 app.post('/tasks', tasksController.create);
 app.delete('/tasks/:id', tasksController.destroy);
 
-app.post('/login', async (req, res) => {
-  // recibir credenciales (email y contraseña) y retornar un JWT
-  const { email, password } = req.body;
-
-  const user = await User.authenticate(email, password);
-  if (user) {
-    // retornamos el JWT
-    const token = jwt.sign({ 
-      exp: Math.floor(Date.now() / 1000) + (15 * 24 * 60 * 60),
-      userId: user._id 
-    }, 'secret key');
-    res.json({ token });
-  } else {
-    res.status(401).json({ error: "invalid-credentials", message: 'Credenciales inválidas' });
-  }
-});
+app.post('/login', authController.login);
+app.get('/me', auth, authController.me);
 
 module.exports = app;

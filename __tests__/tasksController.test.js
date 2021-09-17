71dd72b17797bf009c14569617d1810c10d230db
Germán Escobar <german.escobarc@gmail.com>
2021-09-17 15:52:06 -0500

    Add test with authentication

--- __tests__/tasksController.test.js
+++ __tests__/tasksController.test.js
@@ -1,6 +1,9 @@
 const request = require('supertest')
 const mongoose = require('mongoose');
-const app = require('../app')
+const app = require('../app');
+const User = require('../user');
+const { generateJWT } = require('../utils');
+const Task = require('../task');
 
 afterAll(() => {
   mongoose.disconnect()
@@ -23,6 +26,61 @@ describe('GET /test', () => {
   test('responds with correct JSON', async () => {
     const response = await request(app).get('/test')
     expect(response.body).toEqual({ status: "ok" })
+  })
+})
+
+describe('GET /tasks', () => {
+  let user, token;
+  beforeEach(async () => {
+    user = await User.create({ email: "test@example.com", password: "test1234", firstName: "Pedro", lastName: "Perez" })
+    token = generateJWT(user)
+  })
+
+  test('responds 401 if user not authenticated', async () => {
+    const response = await request(app)
+      .get('/tasks')
+    
+      expect(response.statusCode).toBe(401)
+  })
+
+  test('responds 200 if user is authenticated', async () => {
+    const response = await request(app)
+      .get('/tasks')
+      .set('Authorization', token)
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('responds with and empty list of tasks', async () => {
+    const response = await request(app)
+      .get('/tasks')
+      .set('Authorization', token)
+
+    expect(response.body).toEqual({ count: 0, data: [], page: 1 })
+  })
+
+  test('responds with a list of tasks', async () => {
+    let task1 = await Task.create({ title: "Tarea 1" })
+    const task2 = await Task.create({ title: "Tarea 2" })
+
+    const response = await request(app)
+      .get('/tasks')
+      .set('Authorization', token)
+
+    const { count, data, page } = response.body
+    expect(count).toBe(2)
+    expect(data.length).toBe(2)
+    expect(page).toBe(1)
+
+    //expect(JSON.stringify(data[0])).toEqual(JSON.stringify(task1._doc))
+
+    expect(data[0]._id).toBe(task1._id.toString())
+    expect(data[0].title).toBe(task1.title)
+    expect(data[0].completed).toBe(task1.completed)
+
+    expect(data[1]._id).toBe(task2._id.toString())
+    expect(data[1].title).toBe(task2.title)
+    expect(data[1].completed).toBe(task2.completed)
   })
 })
 

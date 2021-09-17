3d7ff1e1bc247d39376094025055e6fda6eba2e1
Germán Escobar <german.escobarc@gmail.com>
2021-09-17 15:54:38 -0500

    Use generateJWT from login controller

--- authController.js
+++ authController.js
@@ -1,5 +1,6 @@
 const User = require('./user');
 var jwt = require('jsonwebtoken');
+const { generateJWT } = require('./utils');
 
 const login = async (req, res) => {
   // recibir credenciales (email y contraseña) y retornar un JWT
@@ -8,10 +9,7 @@ const login = async (req, res) => {
   const user = await User.authenticate(email, password);
   if (user) {
     // retornamos el JWT
-    const token = jwt.sign({ 
-      exp: Math.floor(Date.now() / 1000) + (15 * 24 * 60 * 60),
-      userId: user._id 
-    }, process.env.SECRET_KEY || 'secret key');
+    const token = generateJWT(user);
     res.json({ token });
   } else {
     res.status(401).json({ error: "invalid-credentials", message: 'Credenciales inválidas' });

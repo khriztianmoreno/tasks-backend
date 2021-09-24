bd261b8a52650fa6eca78548cbebb465193c7dbf
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-09-24 15:30:05 -0500

    Change message

--- api/helloworld/helloworld.controller.js
+++ api/helloworld/helloworld.controller.js
@@ -1,7 +1,7 @@
 // Controller
 
 function index(req, res) {
-  return res.status(200).json({message: 'helloworld'})
+  return res.status(200).json({message: 'helloworld :D amiwis'})
 }
 
 module.exports = {
